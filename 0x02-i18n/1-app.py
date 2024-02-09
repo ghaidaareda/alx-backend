d5652995ec38f1cea3175fddddea97a0d066e090
ghaidaareda <dodo.sa7afy@gmail.com>
2024-02-10 00:09:52 +0200

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,17 +4,22 @@ basic app
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, format_datetime
-from datetime import datetime 
+from datetime import datetime
 
-app = Flask(__name__)
-babel = Babel(app)
 
 class Config:
     """Configuration class for the application."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-@babel.localeselector
-def get_locale():
-        """ get locale"""
-        return request.accept_languages.best_match(Config.LANGUAGES, default=Config.BABEL_DEFAULT_LOCALE)
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello():
+    """ basic  route for / endpoint """
+    return render_template("1-index.html")
