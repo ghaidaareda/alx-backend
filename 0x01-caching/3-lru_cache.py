e9fb10c6e9ba351c5c7e9c6ae2d1753abb637c40
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-10 00:22:22 +0300

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,9 +9,11 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     def __init__(self):
+        """init method"""
         super().__init__()
 
     def put(self, key, item):
+        """ discard the least recently used item (LRU algorithm)"""
         if key is not None and item is not None:
             # If key already exists, move it to the end (most recently used)
             if key in self.cache_data:
@@ -24,6 +26,7 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """ retrieve data"""
         if key is not None and key in self.cache_data:
             # Move the accessed key to the end (most recently used)
             value = self.cache_data.pop(key)
