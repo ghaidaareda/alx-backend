1aaae7291cf1d18a7ff2cd4344f94b97180ac762
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-10 00:19:58 +0300

    3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Create a class LRUCache that inherits from BaseCaching
+and is a caching system
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            # If key already exists, move it to the end (most recently used)
+            if key in self.cache_data:
+                del self.cache_data[key]
+            # If cache is full, remove the least recently used item
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                oldest_key = next(iter(self.cache_data))
+                del self.cache_data[oldest_key]
+                print(f'DISCARD: {oldest_key}')
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            # Move the accessed key to the end (most recently used)
+            value = self.cache_data.pop(key)
+            self.cache_data[key] = value
+            return value
+        return None
